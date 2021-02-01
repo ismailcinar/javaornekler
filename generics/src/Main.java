18bdee8c0e5689d891c9a9d5a53f3b43d0dc1c4e
ismailcinar <ismailcinar99@gmail.com>
2021-02-01 22:38:52 +0300

    java

---
+++ generics/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void  main(String[]args){
+
+       Validator validator=new Validator();
+       Customer customer=new Customer();
+       validator.validate(customer);
+    }
+}
